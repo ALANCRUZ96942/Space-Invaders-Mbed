85a308a652a948e0e1813d04df86dc6b166c9235
ALANCRUZ96942 <alan96942@gmail.com>
2023-05-12 15:32:49 +0200

    fix: delete extra files

--- Proyecto final/PF_pin.h
+++
@@ -1,7 +0,0 @@
-
-#include <LPC17xx.h>
-#include <PIN_LPC17xx.h>
-#include <GPIO_LPC17xx.h>
-
-void initPin_LED(uint32_t port_number, uint32_t pin_number);
-void initPin_JOY(uint32_t port_number, uint32_t pin_number);
